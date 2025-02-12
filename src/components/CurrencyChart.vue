ee305986442d4e7b9f2aad7833dd9e075be7a8f5
Boburjon <shadow88bob@gmail.com>
2025-02-12 16:19:43 +0500

    Update CurrencyChart

--- src/components/CurrencyChart.vue
+++ src/components/CurrencyChart.vue
@@ -1,44 +1,33 @@
 <template>
     <div class="bg-white text-black p-4 rounded-lg shadow-md mt-4">
-        <h2 class="text-xl font-bold text-center mb-4">Exchange Rate Chart</h2>
-        <div id="tradingview-widget" class="h-96"></div>
+        <h2 class="text-xl font-bold text-center mb-4">Exchange Rate Chart</h2> 
+        <select v-model="selectedCurrency" @change="updateChart" class="mb-4 p-2 border rounded">
+            <option v-for="currency in currencies" :key="currency" :value="currency">
+                {{ currency }}
+            </option>
+        </select>
+
+        <div v-if="selectedCurrency">
+            <iframe :key="selectedCurrency"
+                :src="`https://s.tradingview.com/widgetembed/?frameElementId=tradingview_${selectedCurrency}&symbol=FX:${selectedCurrency}USD&interval=D&hidesidetoolbar=1&symboledit=1&saveimage=1&toolbarbg=f1f3f6&studies=[]&theme=light&style=1&timezone=Etc/UTC&studies_overrides={}&overrides={}&enabled_features=[]&disabled_features=[]&locale=en`"
+                width="100%" height="400" frameborder="0" allowfullscreen>
+            </iframe>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: {
-        symbol: {
-            type: String,
-            default: "USD/EUR",
-        },
+    data() {
+        return {
+            selectedCurrency: "USD",
+            currencies: ["USD", "RUB", "UZS", "EUR", "GBP", "JPY", "AUD"]
+        };
     },
-    mounted() {
-        if (!window.TradingView) {
-            console.error("TradingView script yuklanmagan!");
-            return;
+    methods: {
+        updateChart() {
+            console.log(`Tanlangan valyuta: ${this.selectedCurrency}`);
         }
-
-        new window.TradingView.widget({
-            container_id: "tradingview-widget",
-            width: "100%",
-            height: "400px",
-            symbol: this.symbol.replace("/", ""),
-            interval: "D", 
-            timezone: "Etc/UTC",
-            theme: "light",
-            style: "1",
-            locale: "en",
-            toolbar_bg: "#f1f3f6",
-            enable_publishing: false,
-            hide_side_toolbar: true,
-        });
-    },
+    }
 };
 </script>
-
-<style scoped>
-#tradingview-widget {
-    min-height: 400px;
-}
-</style>
