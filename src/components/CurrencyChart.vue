92c4801c56afb951d6fa8c2b920f61cf4b724cfc
Boburjon <shadow88bob@gmail.com>
2025-02-12 16:37:00 +0500

    Update CurrencyChart Fix Errors

--- src/components/CurrencyChart.vue
+++ src/components/CurrencyChart.vue
@@ -2,15 +2,15 @@
     <div class="bg-white text-black p-4 rounded-lg shadow-md mt-4">
         <h2 class="text-xl font-bold text-center mb-4">Exchange Rate Chart</h2>
         <div class="flex justify-center gap-2 mb-4">
-            <button v-for="currency in currencies" :key="currency" @click="selectedCurrency = currency"
+            <button v-for="currency in currencies" :key="currency" @click="updateChart(currency)"
                 class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition">
                 {{ currency }}
             </button>
         </div>
 
-        <div v-if="selectedCurrency">
-            <iframe :key="selectedCurrency"
-                :src="`https://s.tradingview.com/widgetembed/?frameElementId=tradingview_${selectedCurrency}&symbol=FX:${selectedCurrency}USD&interval=D&hidesidetoolbar=1&symboledit=1&saveimage=1&toolbarbg=f1f3f6&studies=[]&theme=light&style=1&timezone=Etc/UTC&studies_overrides={}&overrides={}&enabled_features=[]&disabled_features=[]&locale=en`"
+        <div v-if="selectedSymbol">
+            <iframe :key="selectedSymbol"
+                :src="`https://s.tradingview.com/widgetembed/?frameElementId=tradingview_${selectedSymbol}&symbol=${selectedSymbol}&interval=D&hidesidetoolbar=1&symboledit=1&saveimage=1&toolbarbg=f1f3f6&studies=[]&theme=light&style=1&timezone=Etc/UTC&studies_overrides={}&overrides={}&enabled_features=[]&disabled_features=[]&locale=en`"
                 width="100%" height="400" frameborder="0" allowfullscreen>
             </iframe>
         </div>
@@ -21,9 +21,28 @@
 export default {
     data() {
         return {
-            selectedCurrency: "USD",
+            selectedSymbol: "FX:EURUSD",
             currencies: ["USD", "RUB", "UZS", "EUR", "GBP", "JPY", "AUD"]
         };
+    },
+    methods: {
+        updateChart(currency) {
+            if (currency === "USD") {
+                this.selectedSymbol = "FX:USDCHF";
+            }
+            else if (currency === "RUB") {
+                this.selectedSymbol = "FX_IDC:RUBUSD";
+            }
+            else if (currency === "UZS") {
+                this.selectedSymbol = "FX_IDC:USDUZS";
+            }   
+            else if (currency === "JPY") {
+                this.selectedSymbol = "FX:USDJPY";
+            }
+            else {
+                this.selectedSymbol = `FX:${currency}USD`;
+            }
+        }
     }
 };
 </script>
